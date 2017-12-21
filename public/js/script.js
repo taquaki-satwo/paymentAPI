208552ab741de11f6f987d1724215f54b363c05c
Takaaki Sato <taquaki-satwo@users.noreply.github.com>
2017-12-21 10:16:44 +0900

    change functions

--- public/js/script.js
+++ public/js/script.js
@@ -1,37 +1,42 @@
-var methodData = [{ supportedMethods: ['visa', 'mastercard'] }];
-var details = {
-  displayItems: [
-    {
-      label: '通常商品',
-      amount: { currency: 'YEN', value: '10000' }
-    },
-    {
-      label: 'クーポン',
-      amount: { currency: 'YEN', value: '-2000' }
-    }
-  ],
-  total: {
-    label: '合計',
-    amount: { currency: 'YEN', value: '8000' }
-  }
+window.onload = function () {
+  var $btn = document.getElementById('btn');
+  btn.addEventListener('click', onBuyClicked, false);
 }
 
-var request = new PaymentRequest(methodData, details);
-var btn = document.getElementById('btn');
+function onBuyClicked() {
+  if (!window.PaymentRequest) {
+    location.href = '/checkout';
+    return;
+  }
 
-btn.addEventListener('click', function () {
-  request.show().then(function (PaymentResponse) {
-    paymentResponse.complete('success');
-  }).catch(function (err) {
-    console.error('ERROR', err.message);
-  });
-}, false);
+  var supportedInstruments = [{
+    supportedMethods: ['basic-card'],
+    data: {
+      supportedNetworks: [
+        'visa', 'mastercard', 'amex', 'discover',
+        'diners', 'jcb', 'unionpay'
+      ]
+    }
+  }];
 
-var paymentTimeout = window.setTimeout(function () {
-  window.clearTimeout(paymentTimeout);
-  request.abort().then(function () {
-    console.log('Payment Timeout');
-  }).catch(function () {
-    console.log('Unable');
-  });
-}, 20 * 60 * 1000);
+  var details = {
+    displayItems: [
+      {
+        label: '通常商品',
+        amount: { currency: 'YEN', value: '10000' }
+      },
+      {
+        label: 'クーポン',
+        amount: { currency: 'YEN', value: '-2000' }
+      }
+    ],
+    total: {
+      label: '合計',
+      amount: { currency: 'YEN', value: '8000' }
+    }
+  };
+
+  var request = new PaymentRequest(supportedInstruments, details);
+
+  request.show();
+}
